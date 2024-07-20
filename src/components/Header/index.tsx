58e6789adb6c11edd3ab28853420513cf6e7a86f
danujav <danujagreru@gmail.com>
2024-07-20 13:51:12 +0530

    setting up header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,54 +1,89 @@
-'use client';
+"use client";
 
-import { Autocomplete, Group, Burger, rem } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { IconSearch } from '@tabler/icons-react';
-// import { MantineLogo } from '@mantinex/mantine-logo';
-import classes from './Header.module.css';
-import { IconShoppingCart } from '@tabler/icons-react';
+import {
+  Autocomplete,
+  Group,
+  Burger,
+  rem,
+  Image,
+  ActionIcon,
+  Flex,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconSearch } from "@tabler/icons-react";
+import classes from "./Header.module.css";
+import {
+  IconShoppingCart,
+  IconHome2,
+  IconAdjustments,
+} from "@tabler/icons-react";
+import { NavLink } from "@mantine/core";
+import BookShopLogo from "../../../public/BookShopLogo.png";
+import NextImage from "next/image";
 
 const links = [
-  { link: '/home', label: 'Home' },
-  { link: '/cart', icon: <IconShoppingCart stroke={1.25} /> },
+  { link: "/about", label: "Features" },
+  { link: "/pricing", label: "Pricing" },
+  { link: "/learn", label: "Learn" },
+  { link: "/community", label: "Community" },
 ];
 
-
 export default function Header() {
-
   const [opened, { toggle }] = useDisclosure(false);
 
-  const items = links.map((link, index) => (
+  const items = links.map((link) => (
     <a
-    key={link.label || `icon-${index}`}
+      key={link.label}
       href={link.link}
       className={classes.link}
       onClick={(event) => event.preventDefault()}
     >
-      {typeof link.label === 'string' ? link.label : link.icon}
+      {link.label}
     </a>
   ));
 
   return (
     <header className={classes.header}>
-    <div className={classes.inner}>
-      <Group>
-        <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-        {/* <MantineLogo size={28} /> */}
-      </Group>
-
-      <Group>
-        <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
-          {items}
+      <div className={classes.inner}>
+        <Group>
+          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+          <Image
+            component={NextImage}
+            radius="md"
+            h={150}
+            w="auto"
+            fit="contain"
+            src={BookShopLogo}
+            alt="/"
+          />
         </Group>
-        <Autocomplete
-          className={classes.search}
-          placeholder="Search"
-          leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
-          data={['React', 'Angular', 'Vue', 'Next.js', 'Riot.js', 'Svelte', 'Blitz.js']}
-          visibleFrom="xs"
-        />
-      </Group>
-    </div>
-  </header>
+
+        <Group>
+          <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
+            {items}
+          </Group>
+          <Autocomplete
+            className={classes.search}
+            placeholder="Search"
+            leftSection={
+              <IconSearch
+                style={{ width: rem(16), height: rem(16) }}
+                stroke={1.5}
+              />
+            }
+            data={[
+              "React",
+              "Angular",
+              "Vue",
+              "Next.js",
+              "Riot.js",
+              "Svelte",
+              "Blitz.js",
+            ]}
+            visibleFrom="xs"
+          />
+        </Group>
+      </div>
+    </header>
   );
 }
