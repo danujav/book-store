03639aff333626832906ac890d0a46c6ccb5d325
danujav <danujagreru@gmail.com>
2024-07-21 01:40:15 +0530

    create a custom hooks to fetch the api requests

---
+++ src/utils/useFetchData.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+
+export const useFetchData = (url: string, delay: number) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    fetchData();
+  }, [url, delay]);
+
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      await new Promise(resolve => setTimeout(resolve, delay));
+
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const json = await response.json();
+      console.log(json.books);
+      
+      setData(json.books);
+    } catch (error: any) {
+      setError(error)
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error };
+
+}
