6df730b7d200924851ddfe012047d164668f8538
danujav <danujagreru@gmail.com>
2024-07-21 04:13:44 +0530

    Create Book interface to type safe

---
+++ src/utils/types/Book.ts
@@ -0,0 +1,11 @@
+export default interface Book {
+    id: number,
+    title: string,
+    subtitle: string,
+    isbn13: number,
+    price: string,
+    image: string
+    url: string,
+    author: string,
+    category: string
+}
